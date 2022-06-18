e07eff32c97a97d849a47fae5bf6c4e7c62a39a6
ngzhekai <ngzhekai@gmail.com>
2022-06-18 17:28:23 +0800

    .

--- test.sh
+++ test.sh
@@ -6,11 +6,14 @@ SHA=$(curl \
   -H "Authorization: token $token" \
   https://api.github.com/repos/$owner/$repo/contents/$path | jq '.sha')
 
+TEXT=$(curl https://api.github.com/zen)
+
+# CVT=$(echo -n $TEXT | base64)
+
 curl \
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $token" \
   https://api.github.com/repos/ngzhekai/Green-commit/contents/README.md \
-  -d '{"message":"a new commit message","committer":{"name":"Gilbert Octocat","email":"ngzhekai@gmail.com"},"content":"bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz","sha":'$SHA'}'
-
+  -d "{\'message\':\'Wise man says only fool rush in\',\'committer\':{\'name\':\'McKidKneeKaneSir\',\'email\':\'ngzhekai@gmail.com\'},\'content\':\'TWNLaWRLbmVlS2FuZVNpcg==\',\'sha\':\'$SHA\'}"
 
