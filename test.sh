cf6a986fa5ca40a31ce58f59f59d31b34bd7f5db
ngzhekai <ngzhekai@gmail.com>
2022-06-18 22:48:26 +0800

    .

--- test.sh
+++ test.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+# ++++++++++++++++++++++++++
+#  Green-commit by ngzhekai
+# ++++++++++++++++++++++++++ 
+
+# Get environment variables
 source .env
 
+# Get SHA 
 SHA=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $token" \
-  https://api.github.com/repos/$owner/$repo/contents/$path | jq '.sha')
+  https://api.github.com/repos/$owner/$repo/contents/$path | jq -r '.sha')
 
-# TEXT=$(curl https://api.github.com/zen)
-
-# CVT=$(echo -n $TEXT | base64)
+TEXT=$(curl https://api.github.com/zen)
+CVT=$(echo -n $TEXT | base64)
 
 curl \
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $token" \
-  https://api.github.com/repos/ngzhekai/Green-commit/contents/README.md \
-  -d "{\"message\":\"Wise man says only fool rush in\",\"committer\":{\"name\":\"McKidKneeKaneSir\",\"email\":\"ngzhekai@gmail.com\"},\"content\":\"TWNLaWRLbmVlS2FuZVNpcg==\",\"sha\":\"$SHA\"}"
+  https://api.github.com/repos/$owner/$repo/contents/$path \
+  -d "{\"message\":\"Nothing\",\"committer\":{\"name\":\"Hatsune Miku\",\"email\":\"$email\"},\"content\":\"$CVT\",\"sha\":\"$SHA\"}"
 
