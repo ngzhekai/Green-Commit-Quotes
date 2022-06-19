70970504e770af0a00a5af0586d94693b4587a55
ngzhekai <ngzhekai@gmail.com>
2022-06-19 21:35:50 +0800

    .

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@
 #  Green-commit by ngzhekai
 # ++++++++++++++++++++++++++ 
 
-# Get environment variables
-source .env
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
 
 # Get SHA 
 SHA=$(curl \
