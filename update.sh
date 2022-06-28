eebfa3f9003c2f671602badc2d3ef30844ef1392
ngzhekai <ngzhekai@gmail.com>
2022-06-28 21:39:20 +0800

    .

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ++++++++++++++++++++++++++
+#  Green-Commit-Quote by ngzhekai
+# ++++++++++++++++++++++++++ 
+
+source=$(curl https://zenquotes.io/api/random)
+
+# Get the quote and author
+quote=$(echo $source | jq -r .[].q)
+author=$(echo $source | jq -r .[].a)
+# Concatenate the string
+output="### Commit Quotes <br> <br> <q>${quote}</q> -<em>${author}</em>"
+
+echo $output > README.md
+
+git config --local user.email "ngzhekai@gmail.com"
+git config --local user.name "Hatsune Miku"
+git commit -m "$quote" -a
+
