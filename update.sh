11f563a2290a580b8143e47c9499f047453bb42f
ngzhekai <ngzhekai@gmail.com>
2022-07-07 18:49:57 +0800

    testing

--- update.sh
+++ update.sh
@@ -9,11 +9,13 @@ source=$(curl https://zenquotes.io/api/random)
 quote=$(echo $source | jq -r .[].q)
 author=$(echo $source | jq -r .[].a)
 # Concatenate the string
-output="### Quotes that gets updated every 2hr or new push commit <br> <br>"
-output1="#### <q>${quote}</q> -<em>${author}</em>"
+output="## Green-Commit-Quotes by ngzhekai"
+output1="### <q>${quote}</q> -<em>${author}</em>"
+output2="> These Quotes will be updated on a frequency of 2-3hr"
 
 echo $output > README.md
 echo $output1 >> README.md
+echo $output2 >> README.md
 
 git config --local user.email "ngzhekai@gmail.com"
 git config --local user.name "Hatsune Miku"
