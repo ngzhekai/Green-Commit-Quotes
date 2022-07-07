a4a65788fb8764b8fda50d78a2fdfb687c9bdeba
ngzhekai <ngzhekai@gmail.com>
2022-07-07 18:55:10 +0800

    test

--- update.sh
+++ update.sh
@@ -10,8 +10,8 @@ quote=$(echo $source | jq -r .[].q)
 author=$(echo $source | jq -r .[].a)
 # Concatenate the string
 output="## Get Your Daily Dose of Quotes!"
-output1="### <q>${quote}</q> -<em>${author}</em> <br>"
-output2="#### These Quotes will be updated on a frequency basis of 2-3 hours ~"
+output1="### <q>${quote}</q> -<em>${author}</em> <br><br>"
+output2="##### NOTE: These Quotes will be updated on a frequency basis of 2-3 hours ~"
 
 echo $output > README.md
 echo $output1 >> README.md
